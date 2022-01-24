34af8ce69485c26581fb5c08a371adc6128cee8d
TheWilli67 <william.hertrich@numericable.fr>
2022-01-24 16:56:56 +0100

    aaaaaaa

--- src/data/M&A.py
+++ src/data/M&A.py
@@ -1,9 +1,7 @@
 
+import numpy as np
 import pandas as pd
-# La méthode dropna() permet de supprimer les données manquantes: on peut spécifier l'axe de suppression axis=0 on supprime les lignes où il y a des données manquantes, axis=1 on supprime les colonnes où il y a des données manquantes
-# On peut imputer les valeurs manquantes avec la valeur moyenne ou encore avec la valeur la plus présente dans la colonne.
-dfm = pd.read_csv('youtube-mrg.csv')
-# df.fillna(df.mean())  # moyenne
+import seaborn as sns
 
-pd.concat('youtube-mrg.csv')
-print([dfm])
+data = pd.read_csv('youtube-mrg.csv')
+draft = data.select_dtypes(include=['int'])
