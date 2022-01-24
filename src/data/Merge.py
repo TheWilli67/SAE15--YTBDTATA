e63a471a00a09ad983b279019fd3e45a8725905b
TheWilli67 <william.hertrich@numericable.fr>
2022-01-24 13:14:10 +0100

    part 1 commit end

---
+++ src/data/Merge.py
@@ -0,0 +1,36 @@
+from numpy import delete
+import pandas as pd
+
+df1 = pd.read_csv("data/raw/youtube-1.csv",
+                  sep='\t',
+                  names=["video_id", "trending_date", "title", "channel_title", "category_id", "publish_time", "tags", "views,likes", "dislikes", "comment_count", "thumbnail_link", "comments_disabled", "ratings_disabled", "video_error_or_removed", "description"])
+df2 = pd.read_csv("data/raw/youtube-2.csv",
+                  sep='\t',
+                  names=["video_id", "trending_date", "title", "channel_title", "category_id", "publish_time", "tags", "views,likes", "dislikes", "comment_count", "thumbnail_link", "comments_disabled", "ratings_disabled", "video_error_or_removed", "description"])
+
+df3 = pd.read_csv("data/raw/youtube-3.csv",
+                  sep='\t',
+                  names=["video_id", "trending_date", "title", "channel_title", "category_id", "publish_time", "tags", "views,likes", "dislikes", "comment_count", "thumbnail_link", "comments_disabled", "ratings_disabled", "video_error_or_removed", "description"])
+
+df4 = pd.read_csv("data/raw/youtube-4.csv",
+                  sep='\t',
+                  names=["video_id", "trending_date", "title", "channel_title", "category_id", "publish_time", "tags", "views,likes", "dislikes", "comment_count", "thumbnail_link", "comments_disabled", "ratings_disabled", "video_error_or_removed", "description"])
+
+df5 = pd.read_csv("data/raw/youtube-5.csv",
+                  sep='\t',
+                  names=["video_id", "trending_date", "title", "channel_title", "category_id", "publish_time", "tags", "views,likes", "dislikes", "comment_count", "thumbnail_link", "comments_disabled", "ratings_disabled", "video_error_or_removed", "description"])
+
+
+final_dataframe = pd.concat([df1, df2, df3, df4, df5, ])
+
+print(final_dataframe)
+
+if 'youtube-0.csv' == True:
+    delete('youtube-0.csv')
+
+final_dataframe.to_csv(
+    'youtube-0.csv', index=False)
+
+Cov = pd.read_csv("path/to/file.txt",
+                  sep='\t',
+                  names=["Sequence", "Start", "End", "Coverage"])
