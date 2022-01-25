80b28d9a391f69455a4676fcb3ddf83308e4831c
TheWilli67 <william.hertrich@numericable.fr>
2022-01-25 16:23:35 +0100

    zuiopmuil

--- src/data/Merge.py
+++ src/data/Merge.py
@@ -1,14 +1,17 @@
 from base64 import encode
+from email import header
+from pickle import NONE
 from numpy import delete, true_divide
 import pandas as pd
 # encode = utf_8
-df1 = pd.read_csv("data/raw/youtube-1.csv")
-df2 = pd.read_csv("data/raw/youtube-2.csv")
-df3 = pd.read_csv("data/raw/youtube-3.csv")
-df4 = pd.read_csv("data/raw/youtube-4.csv")
-df5 = pd.read_csv("data/raw/youtube-5.csv")
+df1 = pd.read_csv("data/raw/youtube-1.csv",
+                  sep=',',
+                  names=["video_id", "trending_date", "title", "channel_title", "category_id", "publish_time", "tags", "views,likes", "dislikes", "comment_count", "thumbnail_link", "comments_disabled", "ratings_disabled", "video_error_or_removed", "description"])
+
+df2 = pd.read_csv("data/raw/youtube-2.csv", header=None)
+df3 = pd.read_csv("data/raw/youtube-3.csv", header=None)
+df4 = pd.read_csv("data/raw/youtube-4.csv", header=None)
+df5 = pd.read_csv("data/raw/youtube-5.csv", header=None)
 dataframe = pd.concat([df1, df2, df3, df4])
 print(dataframe)
-if 'mrg.csv' == True:
-    delete('mrg.csv')
 dataframe.to_csv('mrg.csv', index=False)
