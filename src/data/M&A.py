4010147de55eba28ef9f3e1d0b9550456bbf1919
TheWilli67 <william.hertrich@numericable.fr>
2022-01-25 15:26:20 +0100

    aaaaaaaaa

--- src/data/M&A.py
+++ src/data/M&A.py
@@ -1,6 +1,5 @@
-import numpy as np
 import pandas as pd
-import seaborn as sns
 
 data = pd.read_csv('mrg.csv')
-draft = data.select_dtypes(include=['int'])
+#draft = data.select_dtypes(include=['int'])
+df_mask = pd.DataFrame['video_id'] == True
--- src/data/Merge.py
+++ src/data/Merge.py
@@ -1,29 +1,29 @@
 from base64 import encode
-from encodings import utf_8
 import pandas as pd
-import os
-encode = utf_8
+# encode = utf_8
 df1 = pd.read_csv("data/raw/youtube-1.csv",
                   sep=',',
                   names=["video_id", "trending_date", "title", "channel_title", "category_id", "publish_time", "tags", "views,likes", "dislikes", "comment_count", "thumbnail_link", "comments_disabled", "ratings_disabled", "video_error_or_removed", "description"])
+# pd.read_csv lit le fichier dans les ( ) sep = separateur / names c'est les titre des categories
 df2 = pd.read_csv("data/raw/youtube-2.csv",
                   sep=',',
                   names=["video_id", "trending_date", "title", "channel_title", "category_id", "publish_time", "tags", "views,likes", "dislikes", "comment_count", "thumbnail_link", "comments_disabled", "ratings_disabled", "video_error_or_removed", "description"])
-
+# pd.read_csv lit le fichier dans les ( ) sep = separateur / names c'est les titre des categories
 df3 = pd.read_csv("data/raw/youtube-3.csv",
                   sep=',',
                   names=["video_id", "trending_date", "title", "channel_title", "category_id", "publish_time", "tags", "views,likes", "dislikes", "comment_count", "thumbnail_link", "comments_disabled", "ratings_disabled", "video_error_or_removed", "description"])
-
+# pd.read_csv lit le fichier dans les ( ) sep = separateur / names c'est les titre des categories
 df4 = pd.read_csv("data/raw/youtube-4.csv",
                   sep=',',
                   names=["video_id", "trending_date", "title", "channel_title", "category_id", "publish_time", "tags", "views,likes", "dislikes", "comment_count", "thumbnail_link", "comments_disabled", "ratings_disabled", "video_error_or_removed", "description"])
-
+# pd.read_csv lit le fichier dans les ( ) sep = separateur / names c'est les titre des categories
 df5 = pd.read_csv("data/raw/youtube-5.csv",
                   sep=',',
                   names=["video_id", "trending_date", "title", "channel_title", "category_id", "publish_time", "tags", "views,likes", "dislikes", "comment_count", "thumbnail_link", "comments_disabled", "ratings_disabled", "video_error_or_removed", "description"])
+# pd.read_csv lit le fichier dans les ( ) sep = separateur / names c'est les titre des categories
 
+dataframe = pd.concat([df1, df2, df3, df4, df5])
+print(dataframe)
+# print dataframe pour voir ce qu'il va merge dans le fichier .csv
 
-final_dataframe = pd.concat([df1, df2, df3, df4, df5])
-print(final_dataframe)
-
-final_dataframe.to_csv('mrg.csv', index=False)
+dataframe.to_csv('mrg.csv', index=False)
