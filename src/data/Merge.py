ddd1d1f9f01b9ecbf59b42d346ad5ded14ff9112
TheWilli67 <william.hertrich@numericable.fr>
2022-01-25 16:59:48 +0100

    bite extrême

--- src/data/Merge.py
+++ src/data/Merge.py
@@ -11,14 +11,10 @@ df4 = pd.read_csv("data/raw/youtube-4.csv", header=None)
 df5 = pd.read_csv("data/raw/youtube-5.csv", header=None)
 dataframe = pd.concat([df1, df2, df3, df4])
 print(dataframe)
-dataframe.to_csv('mrg.csv', index=True)
-
+dataframe.to_csv('mrg.csv', index=False)
 df = pd.read_csv("mrg.csv")
-
-
 new_header = df.iloc[0]  # grab the first row for the header
-df = df[1:]  # take the data less the header row
-df.columns = new_header  # set the header row as the df header
-
+dataframe = df[1:]  # take the data less the header row
+dataframe.columns = new_header  # set the header row as the df header
 print(dataframe)
-dataframe.to_csv('mrgg.csv', index=False)
+dataframe.to_csv('mrg.csv', index=False)
