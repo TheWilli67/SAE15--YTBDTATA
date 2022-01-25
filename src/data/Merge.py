47ac62ed7d21d4bb2e6575b5aab59d9bc3a78624
TheWilli67 <william.hertrich@numericable.fr>
2022-01-25 16:39:56 +0100

    azert

--- src/data/Merge.py
+++ src/data/Merge.py
@@ -4,13 +4,17 @@ from pickle import NONE
 from numpy import delete, true_divide
 import pandas as pd
 # encode = utf_8
-df1 = pd.read_csv("data/raw/youtube-1.csv")
+df1 = pd.read_csv("data/raw/youtube-1.csv", header=None)
 df2 = pd.read_csv("data/raw/youtube-2.csv", header=None)
 df3 = pd.read_csv("data/raw/youtube-3.csv", header=None)
 df4 = pd.read_csv("data/raw/youtube-4.csv", header=None)
 df5 = pd.read_csv("data/raw/youtube-5.csv", header=None)
 dataframe = pd.concat([df1, df2, df3, df4])
+print(dataframe)
+dataframe.to_csv('mrg.csv', index=False)
+
 df = pd.read_csv("mrg.csv")
+
 
 new_header = df.iloc[0]  # grab the first row for the header
 df = df[1:]  # take the data less the header row
@@ -18,4 +22,4 @@ df.columns = new_header  # set the header row as the df header
 
 
 print(dataframe)
-dataframe.to_csv('mrg.csv', index=False)
+dataframe.to_csv('mrgg.csv', index=False)
