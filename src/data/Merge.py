92bf1f0199c2ea9339ad5ee2a1ffa2c9e5c27d24
TheWilli67 <william.hertrich@numericable.fr>
2022-01-25 19:13:43 +0100

    forget
    j'ai oublié df5 merde

--- src/data/Merge.py
+++ src/data/Merge.py
@@ -1,7 +1,3 @@
-from base64 import encode
-from email import header
-from pickle import NONE
-from numpy import delete, true_divide
 import pandas as pd
 # encode = utf_8
 df1 = pd.read_csv("data/raw/youtube-1.csv", header=None)
@@ -9,7 +5,7 @@ df2 = pd.read_csv("data/raw/youtube-2.csv", header=None)
 df3 = pd.read_csv("data/raw/youtube-3.csv", header=None)
 df4 = pd.read_csv("data/raw/youtube-4.csv", header=None)
 df5 = pd.read_csv("data/raw/youtube-5.csv", header=None)
-dataframe = pd.concat([df1, df2, df3, df4])
+dataframe = pd.concat([df1, df2, df3, df4, df5])
 print(dataframe)
 dataframe.to_csv('mrg.csv', index=False)
 df = pd.read_csv("mrg.csv")
