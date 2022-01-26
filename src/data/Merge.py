9e95be18e188ff8946d0fa1181a945b515ffabe8
TheWilli67 <william.hertrich@numericable.fr>
2022-01-26 10:27:32 +0100

    columns

--- src/data/Merge.py
+++ src/data/Merge.py
@@ -6,11 +6,12 @@ df3 = pd.read_csv("data/raw/youtube-3.csv", header=None)
 df4 = pd.read_csv("data/raw/youtube-4.csv", header=None)
 df5 = pd.read_csv("data/raw/youtube-5.csv", header=None)
 dataframe = pd.concat([df1, df2, df3, df4, df5])
-print(dataframe)
 dataframe.to_csv('mrg.csv', index=False)
-df = pd.read_csv("mrg.csv")
-new_header = df.iloc[0]  # grab the first row for the header
-dataframe = df[1:]  # take the data less the header row
+print('mrg.csv')
+dataframe.drop(dataframe.columns[15], axis=1, inplace=True)#dropping the column n°15 (description)
+dataframe.drop(dataframe.columns[13], axis=1, inplace=True)# dropping the other usless column the "thumbnail_link" one
+new_header = dataframe.iloc[0]  # grab the first row for the header
+dataframe = dataframe[1:]  # take the data less the header row
 dataframe.columns = new_header  # set the header row as the df header
-print(dataframe)
 dataframe.to_csv('mrg.csv', index=False)
+print(dataframe)
