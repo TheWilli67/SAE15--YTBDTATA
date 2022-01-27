0b0e6f6f225f91d7fc9c5d451c9fbc1199dd1d22
julieniut <92865222+julieniut@users.noreply.github.com>
2022-01-27 11:52:43 +0100

    create Moyenne dislakes
    test

---
+++ src/data/Moyenne_dislikes.py
@@ -0,0 +1,12 @@
+import pandas as pd
+def ComputeMean():
+    dataframe = pd.read_csv('cleaned/file_merged.csv')
+    l=dataframe['dislikes']
+    l=pd.DataFrame(l)
+    print(l) #affiche la colone likes
+    means=l.mean() #calcule la moyenne
+    means=int(means)
+    print("Means of Each Column:")
+    print(means) #affiche la moyenne des likes
+    return ""
+print(ComputeMean())
