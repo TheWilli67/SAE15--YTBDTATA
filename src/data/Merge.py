25560ac95c2fa52b3d4d7dad7e2c7757805efe30
TheWilli67 <william.hertrich@numericable.fr>
2022-01-27 11:32:08 +0100

    aaaaaaaaa

--- src/data/Merge.py
+++ src/data/Merge.py
@@ -12,13 +12,11 @@ dataframe.to_csv('cleaned/file_merged.csv', index=False)
 
 dataframe.drop(dataframe.columns[15], axis=1, inplace=True)
 dataframe.drop(dataframe.columns[13], axis=1, inplace=True)# dropping the other usless column the "thumbnail_link" one
+dataframe.drop(dataframe.columns[0], axis=0, inplace=True)
+dataframe = dataframe.set_axis(["video_id", "trending_date", "title", "channel_title", "category_id", "publish_time", "tags", "views", "likes", "dislikes",
+                                "comment_count", "comments_disabled", "ratings_disabled", "video_error_or_removed"], axis=1, inplace=False)
 
-del0 = dataframe.iloc[1]  # grab the first row for the header
-dataframe = dataframe[2:]  # take the data less the header row
-dataframe.columns = del0  # set the header row as the df header
 
-dataframe = dataframe.set_axis(["video_id", "trending_date", "title", "channel_title", "category_id", "publish_time", "tags", "views","likes", "dislikes",
-                  "comment_count", "comments_disabled", "ratings_disabled", "video_error_or_removed"], axis=1, inplace=False)
 dataframe.to_csv('cleaned/file_merged.csv', index=False)
 print(dataframe)
 
--- src/data/aberation.py
+++ src/data/aberation.py
@@ -6,6 +6,5 @@ import pandas as pd
 dataframe = pd.read_csv('cleaned/file_merged.csv')
 dataframe = dataframe[dataframe['video_id'].notna()]
 #dataframe = dataframe[dataframe['ratings_disabled'].notna()]
-
 print(dataframe)
 dataframe.to_csv('cleaned/abb.csv', index=False)
