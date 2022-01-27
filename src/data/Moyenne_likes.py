12af6a38dd56915fa725a6e918811667207cb322
julieniut <92865222+julieniut@users.noreply.github.com>
2022-01-27 11:38:07 +0100

    aaaa

--- src/data/Moyenne_likes.py
+++ src/data/Moyenne_likes.py
@@ -4,5 +4,6 @@ l=dataframe['likes']
 l=pd.DataFrame(l)
 print(l) #affiche la colone likes
 means=l.mean() #calcule la moyenne
+means=int(means)
 print("Means of Each Column:")
 print(means) #affiche la moyenne des likes
