f946ae5fa807b069c5473d1c8d330ee448b8ec39
TheWilli67 <william.hertrich@numericable.fr>
2022-01-27 09:01:51 +0100

    aaaaaaaaaaaaa

--- src/data/top_5_likes.py
+++ src/data/top_5_likes.py
@@ -1,6 +1,7 @@
 import pandas as pd
 dataframe = pd.read_csv('cleaned/file_merged.csv')
-ff = dataframe.sort_values('likes', ascending=False)[:5]
-ff.to_csv('cleaned/top_5.csv', index=True, header=None)
+ff = dataframe.sort_values('dislikes', ascending=False)[:4]
+dataframe.drop(dataframe.columns['video_id'], axis=1, inplace=True)
+ff.to_csv('cleaned/top_5.csv')
 print(ff)
 
