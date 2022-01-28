7055f9a82f1035e92b20d836b83de7f0c33d3e16
julieniut <92865222+julieniut@users.noreply.github.com>
2022-01-28 10:02:47 +0100

    camenber

--- src/data/graphique des like et dislikes.py
+++ src/data/graphique des like et dislikes.py
@@ -1,17 +1,26 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-#df = pd.read_csv('cleaned/file_merged.csv')
+df = pd.read_csv('cleaned/file_merged.csv')
 #df[['likes', 'dislikes']].plot(
-   #xlabel='Nombres de vidéos',
-  #title='Likes vs Dislikes')
-
+ # xlabel='Nombres de vidéos',
+ # title='Likes vs Dislikes')
 #plt.show()
-dataframe = pd.read_csv('cleaned/file_merged.csv')
-df=dataframe['like']
-df=pd.DataFrame(df)
-print(df)
-df[df[["likes"] > 700000 ]].plot(
-   xlabel='Nombres de vidéos',
-   title='Likes vs Dislikes')
-
+likes=df['likes']
+dislikes=df['dislikes']
+comparateur=0
+longueurdf=len(df)
+for index, row in df.iterrows():
+    if row['likes'] > row['dislikes']:
+        comparateur+=1
+#print (comparateur)
+comparateur=40723-comparateur
+plt.figure(figsize = (8, 8))
+x = [39500,comparateur]
+plt.pie(x, labels = ['Likes', 'Dislikes'],
+           colors = ['red', 'green'],
+           explode = [0, 0.2],
+           autopct = lambda x: str(round(x, 2)) + '%',
+           pctdistance = 0.7, labeldistance = 1.4,
+           shadow = False)
+plt.legend()
 plt.show()
